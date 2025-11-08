7ada1b1645398ac5c8819a3e60917a5d7d737938
Emmanuel <nwekeemmanuel667@gmail.com>
2025-11-08 15:14:09 +0100

    Prepare project for Render deployment

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d*z5bfrqlp90dic=&ozfxy_@^c!3r$wzzi*&4d8qausnwb1*d9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://asma-cooking.onrender.com']
 
 
 # Application definition
